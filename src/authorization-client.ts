6c0d6cef89c535185dd28bfaf14dc7524f363e7d
Ryan Gomoto <ryan@cyrusbio.com>
2018-12-12 03:37:49 -0800

    Pass options to AuthorizationClient

--- src/authorization-client.ts
+++ src/authorization-client.ts
@@ -1,3 +1,13 @@
 export class AuthorizationClient {
+  private _options: IAuthorizationClientOptions;
 
+  constructor(options: IAuthorizationClientOptions) {
+    this._options = options;
+  }
+}
+
+interface IAuthorizationClientOptions {
+  clientId: string;
+  clientSecret: string;
+  domain: string;
 }
