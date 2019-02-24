3f1313ac42f446e0558bdb40172ec99f0dea83d3
Ryan Gomoto <ryan@cyrusbio.com>
2019-02-23 19:22:53 -0800

    Fix environment variable name

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,7 +3,7 @@ import { getAccessTokenSpec } from './common/get-access-token.spec';
 
 // Generate test options
 const options: TestOptions = {
-  skipMocks: process.env.TEST_CHARGIFY_SKIP_MOCKS === 'true', // default false (use mocks)
+  skipMocks: process.env.SKIP_MOCKS === 'true', // default false (use mocks)
   getAccessToken: {
     audience: process.env.AUTH0_AUDIENCE || 'http://audience',
     clientId: process.env.AUTH0_CLIENT_ID || 'testClientId',
